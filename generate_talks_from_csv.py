418603540bdfcd6180f913b7e1029b65a9e8d4b7
ccarissimo <N20kztu!>
2024-12-20 18:02:31 +0100

    adjust speakers

--- generate_talks_from_csv.py
+++ generate_talks_from_csv.py
@@ -13,33 +13,33 @@ First Name, Last Name, Tentative Title, virtual
 """
 
 for i, row in df.iterrows():
+    if len(str(row['Last Name'])) < 1:  # exclude empty string last names
+        virtual_tag = ""
+        # print(row["virtual"])
+        if str(row['virtual']) == "True":
+            virtual_tag = " (v)"
 
-    virtual_tag = ""
-    # print(row["virtual"])
-    if str(row['virtual']) == "True":
-        virtual_tag = " (v)"
+        talk_title = str(row['Tentative Title']).replace(":", " -")
+        if talk_title == "tbd":
+            talk_title = f"tbd - {row['First Name']} {row['Last Name']}"
 
-    talk_title = str(row['Tentative Title']).replace(":", " -")
-    if talk_title == "tbd":
-        talk_title = f"tbd - {row['First Name']} {row['Last Name']}"
+        talk_title = talk_title + virtual_tag
 
-    talk_title = talk_title + virtual_tag
+        string = f"--- " \
+                 f"\nname: {talk_title}" \
+                 f"\nspeakers: " \
+                 f"\n  - {row['First Name']} {row['Last Name']}" \
+                 f"\ncategories:" \
+                 f"\n  - Presentation" \
+                 f"\n---" \
+                 f"\n\n{talk_title}"
 
-    string = f"--- " \
-             f"\nname: {talk_title}" \
-             f"\nspeakers: " \
-             f"\n  - {row['First Name']} {row['Last Name']}" \
-             f"\ncategories:" \
-             f"\n  - Presentation" \
-             f"\n---" \
-             f"\n\n{talk_title}"
+        path = f"_talks/{row['First Name']}_{row['Last Name']}.md"
 
-    path = f"_talks/{row['First Name']}_{row['Last Name']}.md"
-
-    if path not in existing_files:
-        f = open(path, "a")
-        f.write(string)
-        f.close()
+        if path not in existing_files:
+            f = open(path, "a")
+            f.write(string)
+            f.close()
 
 # print("There are", len(confirmed_speakers), "confirmed speakers")
 # print("There are", len(specified_talks), "specified talks")
